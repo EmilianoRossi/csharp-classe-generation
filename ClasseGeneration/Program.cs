741dd972ddfc524697b81ace9ebf6f9aabfbc06d
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-06 16:16:03 +0200

    funzione aggiungi alunno da console terminata

--- ClasseGeneration/Program.cs
+++ ClasseGeneration/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//ESERCIZIO CLASSE GENERATION
+
+//----------- VARIABILI GLOBALI --------------
+int numeroPostiADisposizione = 0;
+int massimoNumeriPosti = 5;
+
+string[] nomeAlunni = new string[massimoNumeriPosti];
+string[] cognomeAlunni = new string[massimoNumeriPosti];
+int[] annoDiProduzione = new int[massimoNumeriPosti];
+
+
+//----------- DICHIAMO LE MIE FUNZIONI ------------
+void AggiungiAlunno(string nome, string cognome, int età)
+{
+    if (numeroPostiADisposizione < massimoNumeriPosti)
+    {
+        nomeAlunni[numeroPostiADisposizione] = nome;
+        cognomeAlunni[numeroPostiADisposizione] = cognome;
+        annoDiProduzione[numeroPostiADisposizione] = età;
+        numeroPostiADisposizione++;
+
+    }
+    else
+    {
+        Console.WriteLine("Posti in aula disponibili terminati");
+    }
+
+}
